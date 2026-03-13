fed3d60b5104da31e3d6de36e0c2bccf07f85749
Nicolas-Ballesteros07 <automatizacion@solutionsandpayroll.com>
2026-03-13 11:23:39 -0500

    fix wsgil

--- APP/APP/wsgi.py
+++ APP/APP/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for APP project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'APP.settings')
+# Esto agrega la raíz del proyecto al path de búsqueda de Python
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'APP.APP.settings')
 
 application = get_wsgi_application()
